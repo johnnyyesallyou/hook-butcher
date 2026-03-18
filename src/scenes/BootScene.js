d891fefe495cf12f1133ec7e0f81f661211aa25c
johnnyyesallyou <johnnyyesallyou@gmail.com>
2026-03-18 23:47:18 +0300

    Create BootScene.js

---
+++ src/scenes/BootScene.js
@@ -0,0 +1,12 @@
+// src/scenes/BootScene.js
+import Phaser from 'phaser';
+
+export default class BootScene extends Phaser.Scene {
+    constructor() {
+        super('Boot');
+    }
+
+    create() {
+        this.scene.start('Preloader');
+    }
+}
